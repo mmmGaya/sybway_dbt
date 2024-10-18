62da30dd121899cc8f0a64c2e8c9ef1e6b502dc9
bbhkls <kimliserp@gmail.com>
2024-10-18 16:08:51 +0300

    ods_cut change

--- subway_project/models/example/ods_client_cut.sql
+++ subway_project/models/example/ods_client_cut.sql
@@ -1,5 +1,5 @@
 {{ config(materialized='table') }}
 
---select * from ods_client_csv where execution_date = '{{ var('execution_date') }}'
-select * from ods_client_csv where execution_date = (select execution_date from metadata_airflow_test where source_n = 'csv')
+select * from ods_client_csv where execution_date = '{{ var('execution_date') }}'
+-- select * from ods_client_csv where execution_date = (select execution_date from metadata_airflow_test where source_n = 'csv')
 -- select '{{ var('execution_date') }}'
