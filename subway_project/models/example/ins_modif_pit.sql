1a8ece6b6236f96c71b60b8dfd268b74c4d3c18c
bbhkls <kimliserp@gmail.com>
2024-10-30 11:56:03 +0300

    upd pit and dim table

--- subway_project/models/example/ins_modif_pit.sql
+++ subway_project/models/example/ins_modif_pit.sql
@@ -7,6 +7,6 @@ select '{{ var('run_id') }}' dataflow_id,
 from (
 select ac.client_rk client_rk, max(sc.valid_from_dttm) mx_dt
 from dbt_schema."GPR_BV_A_CLIENT" ac join dbt_schema."GPR_RV_S_CLIENT" sc on ac.x_client_rk = sc.client_rk and sc.delete_flg <> 1 and sc.actual_flg = 1
-group by  ac.client_rk) 
+group by  ac.client_rk) tb
 where client_rk in (select client_rk from dbt_schema."GPR_BV_P_CLIENT") 
   and mx_dt > (select max (client_subway_star_vf_dttm) from dbt_schema."GPR_BV_P_CLIENT" where client_rk = tb.client_rk)
--- subway_project/models/example/ins_to_dim.sql
+++ subway_project/models/example/ins_to_dim.sql
@@ -10,4 +10,4 @@ from dbt_schema."GPR_BV_P_CLIENT" pc
 join dbt_schema."GPR_BV_A_CLIENT" ac on pc.client_rk = ac.client_rk 
 join dbt_schema."GPR_RV_S_CLIENT" sc on ac.x_client_rk = sc.client_rk and pc.valid_from_dttm = sc.valid_from_dttm 
 where extract(year from pc.valid_from_dttm) > 1960 and sc.delete_flg = 0 and sc.actual_flg = 1)
-where mrk = srk and (client_rk, valid_from_dttm) not in (select client_rk, valid_from_dttm from dbt_schema."GPR_EM_DIM_CLIENT");
+where mrk = srk and (client_rk, valid_from_dttm) not in (select client_rk, valid_from_dttm from dbt_schema."GPR_EM_DIM_CLIENT")
